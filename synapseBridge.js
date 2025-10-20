f51d79dff9f4db694fcbe64891cc1feeacaaa4ee
Daniel57930 <danieljrich169@gmail.com>
2025-10-20 15:11:38 -0400

    Add Synapse bridge integration module

--- synapseBridge.js
+++ synapseBridge.js
@@ -1,1 +1,13 @@
-// Content for synapseBridge.js
+/**
+ * Synapse Protocol API Integration
+ * Docs: https://docs.synapseprotocol.com/
+ */
+export async function getSynapseQuote({ fromChain, toChain, asset, amount }) {
+  // TODO: Implement REST API call to Synapse for quote
+  // Example (pseudo): fetch('https://api.synapseprotocol.com/quote', { ... })
+  return { fee: 2.8, min: 1, max: 60000, bridge: "Synapse" }; // pseudo
+}
+export async function bridgeSynapse({ fromChain, toChain, asset, amount, wallet }) {
+  // TODO: Implement transfer call
+  return { ok: true, tx: "0xSYNAPSE" };
+}
