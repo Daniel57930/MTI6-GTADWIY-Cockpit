8a69cb26a6717e3b72046980704c65dd8527e9e0
Daniel57930 <danieljrich169@gmail.com>
2025-10-21 05:57:19 -0400

    Create connectMetaMask.js module for MetaMask connection

---
+++ modules/Wallets/connectMetaMask.js
@@ -0,0 +1,13 @@
+export async function connectMetaMask() {
+  if (window.ethereum) {
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      return accounts[0]; // Returns first connected wallet address
+    } catch (error) {
+      throw new Error("MetaMask sign-in failed: " + error.message);
+    }
+  } else {
+    throw new Error("MetaMask extension not detected.");
+  }
+}
